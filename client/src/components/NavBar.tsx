138a5983e81bcd7c6886a59de8fafaf23b5bdd0b
Florentin Monteil <flo.monteil@gmail.com>
2025-01-09 16:03:01 +0100

    feat: create burger menu and add style to the nav bar and home page

--- client/src/components/NavBar.tsx
+++ client/src/components/NavBar.tsx
@@ -1,10 +1,60 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
+import "../styles/NavBar.css";
+import homeIcon from "../assets/images/home-icon.svg";
+import loginIcon from "../assets/images/login-icon.svg";
 
 export default function NavBar() {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
-    <nav>
-      <Link to="/">Accueil</Link>
-      <Link to="/prizes">Récompenses</Link>
+    <nav className="nav-bar-container">
+      <div className="nav-bar-left">
+        <Link className="nav-bar-link" to="/">
+          <img className="home-icon" src={homeIcon} alt="" />
+        </Link>
+      </div>
+      <div className="nav-bar-center">
+        <button
+          type="button"
+          className={`burger-menu-button ${isMenuOpen ? "open" : ""}`}
+          onClick={toggleMenu}
+        >
+          <span className="burger-bar" />
+          <span className="burger-bar" />
+          <span className="burger-bar" />
+        </button>
+        <button
+          type="button"
+          className={`burger-menu-button-text ${isMenuOpen ? "open" : ""}`}
+          onClick={toggleMenu}
+        >
+          Menu
+          <span className="burger-bar" />
+          <span className="burger-bar" />
+          <span className="burger-bar" />
+        </button>
+      </div>
+      <div className="nav-bar-right">
+        <Link className="nav-bar-link" to="/login">
+          <img className="login-icon" src={loginIcon} alt="" />
+        </Link>
+      </div>
+
+      {isMenuOpen && (
+        <div className="burger-menu-dropdown">
+          <Link className="burger-menu-link" to="/prizes" onClick={toggleMenu}>
+            Récompenses
+          </Link>
+          <Link className="burger-menu-link" to="/games" onClick={toggleMenu}>
+            Jeux
+          </Link>
+        </div>
+      )}
     </nav>
   );
 }
