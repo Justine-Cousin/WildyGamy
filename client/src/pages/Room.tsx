16e26884152b7fd69135ebc5da8d187b748bde3d
CharlotteAlbouy <albouy.charlotte@gmail.com>
2025-01-15 11:22:09 +0100

    feat: form contact us

--- client/src/pages/Room.tsx
+++ client/src/pages/Room.tsx
@@ -1,4 +1,5 @@
 import "../styles/Room.css";
+import { useState } from "react";
 import Game1 from "../assets/images/game1.jpg";
 import Game2 from "../assets/images/game2.jpg";
 import Game3 from "../assets/images/game3.jpg";
@@ -14,23 +15,17 @@ function RoomDescription() {
       <img className="room-image" src={room} alt="room-image" />
       <h1 className="room-title">Wildy Gamy</h1>
       <p>
-        Wildy Gamy est un lieu unique en son genre à Toulouse. À l'image de ses
-        créateurs, Justine, Charlotte, Abdou et Florentin, ce temple du gaming
-        nouvelle génération réinvente l'expérience arcade. Plongez dans un
-        univers où le virtuel rencontre le réel : jouez en ligne depuis chez
-        vous, gagnez des points et venez les échanger contre des sessions de jeu
-        exclusives dans notre salle.
+        Né de l'imagination de quatre passionnés Justine, Charlotte, Abdou et
+        Florentin, Wildy Gamy redéfinit l'expérience arcade à Toulouse. Ce lieu
+        unique mêle jeu en ligne et physique : gagnez des points depuis chez
+        vous et échangez-les contre des sessions sur place. Entre jeux rétro,
+        dernières nouveautés et espace lounge équipé, cet établissement est
+        devenu le repaire incontournable des gamers toulousains.
+      </p>
+      <p>
+        8 Rue de Valenciennes
         <br />
-        Notre espace arcade combine le meilleur des deux mondes : une collection
-        de jeux rétro et dernière génération, des bornes d'arcade classiques, et
-        un système de récompenses innovant. Détendez-vous dans notre espace
-        lounge avec une sélection de snacks et de boissons rafraîchissantes
-        pendant vos sessions de jeu.
-        <br />
-        Découvrez Wildy Gamy, là où la communauté gaming toulousaine se retrouve
-        pour partager des moments inoubliables.
-        <br />
-        Rejoignez l'aventure en ligne et sur place !
+        31000 Toulouse
       </p>
       <p>
         Mardi au Dimanche : 10:30 - 1:00
@@ -95,6 +90,125 @@ function RoomGameOnline() {
   );
 }
 
+interface FormData {
+  name: string;
+  email: string;
+  subject: string;
+  message: string;
+}
+
+interface FormErrors {
+  name?: string;
+  email?: string;
+  subject?: string;
+  message?: string;
+}
+
+function RoomForm() {
+  const [formData, setFormData] = useState<FormData>({
+    name: "",
+    email: "",
+    subject: "",
+    message: "",
+  });
+
+  const [errors, setErrors] = useState<FormErrors>({});
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>,
+  ) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    const newErrors: FormErrors = {};
+    if (!formData.name.trim()) {
+      newErrors.name = "Le Nom est requis";
+    }
+
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    if (!formData.email.trim() || !emailPattern.test(formData.email)) {
+      newErrors.email = "L'email est requis et doit être valide";
+    }
+
+    if (!formData.subject.trim()) {
+      newErrors.subject = "Le sujet est requis";
+    }
+
+    if (!formData.message.trim()) {
+      newErrors.message = "Le message est requis";
+    }
+
+    if (Object.keys(newErrors).length > 0) {
+      setErrors(newErrors);
+    } else {
+      setErrors({});
+      alert("Formulaire envoyé avec succès !");
+      setFormData({ name: "", email: "", subject: "", message: "" });
+    }
+  };
+
+  return (
+    <div className="form-container">
+      <h2 className="form-title">Contactez-nous</h2>
+      <form id="myForm" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Votre Nom</label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+            placeholder="ex : DUPONT"
+          />
+          {errors.name && <span className="error">{errors.name}</span>}
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Adresse email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+            placeholder="ex : lorem.ipsum@gmail.com"
+          />
+          {errors.email && <span className="error">{errors.email}</span>}
+        </div>
+        <div className="form-group">
+          <label htmlFor="subject">Sujet</label>
+          <input
+            type="text"
+            id="subject"
+            name="subject"
+            value={formData.subject}
+            onChange={handleChange}
+            placeholder="ex : réservation"
+          />
+          {errors.subject && <span className="error">{errors.subject}</span>}
+        </div>
+        <div className="form-group">
+          <label htmlFor="message">Votre message</label>
+          <textarea
+            id="message"
+            name="message"
+            value={formData.message}
+            onChange={handleChange}
+            placeholder="ex : Bonjour"
+          />
+          {errors.message && <span className="error">{errors.message}</span>}
+        </div>
+        <button className="form-button" type="submit">
+          Envoyer
+        </button>
+      </form>
+    </div>
+  );
+}
+
 function Room() {
   return (
     <div className="room-page">
@@ -103,6 +217,7 @@ function Room() {
         <RoomDescription />
         <RoomCarousel />
         <RoomGameOnline />
+        <RoomForm />
       </div>
     </div>
   );
