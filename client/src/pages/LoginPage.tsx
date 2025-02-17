fbba3999ed1d0b21d7f1e6689802183d43a67fc3
abdoukhoudoss <cisse.abdoukhoudoss@gmail.com>
2025-02-17 09:49:03 +0100

    fix: login page desktop version

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -1,8 +1,8 @@
 import { useSearchParams } from "react-router-dom";
+import LoginPagePicture from "../assets/images/login-page-picture.jpg";
 import logoWG from "../assets/images/logo_wildy_gamy.png";
 import CreateLogin from "../components/CreateLogin";
 import LoginForm from "../components/LoginForm";
-
 import "../styles/LoginPage.css";
 
 export default function LoginPage() {
@@ -11,10 +11,19 @@ export default function LoginPage() {
 
   return (
     <div className="loginpage-container">
-      <img className="games-logo" src={logoWG} alt="Logo" />
-      <div className="loginpage-content">
-        <LoginForm resetToken={token} />
-        <CreateLogin />
+      <img className="games-logo" src={logoWG} alt="Logo Wildy Gamy" />
+      <div className="loginpage-left">
+        <img
+          className="background-image"
+          src={LoginPagePicture}
+          alt="Background"
+        />
+      </div>
+      <div className="loginpage-right">
+        <div className="loginpage-content">
+          <LoginForm resetToken={token} />
+          <CreateLogin />
+        </div>
       </div>
     </div>
   );
